518d7fc1a250538f670eda22262f117aa5f51146
Anna shabbagh <afreenshabbagh@gmail.com>
2024-11-26 13:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create an object 'student' with a property 'name'
+const student = {
+    name: 'Alice'
+};
+
+// Add a method 'getKeys' to the Object prototype
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+// Call the getKeys method on the 'student' object
+console.log(student.getKeys()); // Output: ['name']
